7ec59509f02d2b370a0e7d412b227ec783b3c487
George <user@Roamtechs-MacBook-Pro.local>
2022-02-10 18:15:36 +0300

    implemented redux toolkit for state

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-# Getting Started with Create React App
+# Mindforce Code interview
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). \
 
-## Available Scripts
+Redux toolkit for state management [Redux toolkit](https://redux-toolkit.js.org/)
+
+Tailwindcss 3 for styling [Tailwindcss](https://tailwindcss.com/)
+
+## How to get the project up and running
 
 In the project directory, you can run:
 
-### `npm start`
+### `yarn`
+
+To install all the dependancies .\
+
+### `yarn start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
@@ -39,32 +47,6 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
 
-## Learn More
+## Thank You
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+In case of any questions feel free to get in touch
--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import "./App.css";
-import { useFetch } from "./useFetch";
-import { useState } from "react";
+import "./App.scss";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchData,
+  updateById,
+  updateAll,
+  resetAll,
+  updateEditing,
+} from "./redux/dataSlice";
 
-const InLineEdit = ({ title, setTitle }) => {
-  const [editingValue, setEditingValue] = useState(title);
+// Create inline editable input for every title
+const InLineEdit = ({ title, id }) => {
+  const [editingTitle, setEditingTitle] = useState(title);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setEditingTitle(title);
+  }, [title]);
 
   const onChange = (e) => {
-    setEditingValue(e.target.value);
+    setEditingTitle(e.target.value);
+    // update editing redux state
+    dispatch(updateEditing(true));
   };
 
   const onKeyDown = (event) => {
@@ -16,19 +31,16 @@ const InLineEdit = ({ title, setTitle }) => {
   };
 
   const onBlur = (event) => {
-    if (event.target.value.trim() === "") {
-      setTitle(title);
-    } else {
-      setTitle(event.target.value);
-    }
+    setEditingTitle(title);
+    dispatch(updateById({ title: editingTitle, id: id }));
   };
 
   return (
     <input
       type="text"
-      className="w-full hover:bg-slate-100 hover:border-dashed hover:border-y hover:border-slate-200"
+      className="w-full capitalize hover:bg-slate-100 hover:border-dashed hover:border-y hover:border-slate-200"
       aria-label="Title field"
-      value={editingValue}
+      value={editingTitle}
       onChange={onChange}
       onKeyDown={onKeyDown}
       onBlur={onBlur}
@@ -36,18 +48,20 @@ const InLineEdit = ({ title, setTitle }) => {
   );
 };
 
+// List component
 function PhotoList(props) {
-  const [title, setTitle] = useState("");
-
-  const listPhotos = props.photos.map((photo) => (
+  // Get data from redux store
+  const { data } = useSelector((state) => state.data);
+  // create a list of photos
+  const listPhotos = data.map((photo) => (
     <li
       key={photo.id}
       className="bg-white w-full flex shadow-sm even:bg-slate-50 ring-1 ring-slate-200"
     >
       <img className="h-full" src={photo.thumbnailUrl} alt={photo.title} />
       <div className="w-full py-2 px-2">
-        <InLineEdit title={photo.title} setTitle={setTitle} />
-        <div>{Date.now()}</div>
+        <InLineEdit title={photo.title} id={photo.id} />
+        <div className="ml-2">{Date.now()}</div>
       </div>
     </li>
   ));
@@ -55,20 +69,49 @@ function PhotoList(props) {
   return <ul className="flex flex-wrap flex-column">{listPhotos}</ul>;
 }
 
+// Main app component
 function App() {
-  console.log("App rendering ...");
-  const { data } = useFetch(
-    "https://jsonplaceholder.typicode.com/photos?_limit=100"
-  );
+  const dispatch = useDispatch();
 
-  console.log(data);
+  //fetch data once from server and update state§
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
+  const handleReset = (e) => {
+    dispatch(resetAll());
+  };
+
+  //Get statuses from redux store
+  const { loading, editing } = useSelector((state) => state.data);
+
   return (
     <div className="bg-slate-100">
-      <header className="App-header w-full py-7 text-center text-2xl">
+      <header className="App-header items-center container mx-auto justify-items-center w-full py-7 justify-between flex text-2xl">
         100 photos from jsonplaceholder
+        <div>
+          <button
+            className="bg-blue-600 transition-all hover:bg-blue-700 mr-4 text-white font-bold py-2 px-4 rounded disabled:bg-slate-400"
+            disabled={!editing ? true : false}
+            onClick={() => dispatch(updateAll())}
+          >
+            Update
+          </button>
+          <button
+            className="bg-red-300 transition-all hover:bg-red-400 text-white font-bold py-2 px-4 rounded disabled:bg-slate-300 disabled:text-slate-600"
+            disabled={!editing ? true : false}
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        </div>
       </header>
       <main className="container mx-auto">
-        {data ? <PhotoList photos={data} /> : "Loading..."}
+        {loading ? (
+          <div className="w-full text-center text-xl">Loading...</div>
+        ) : (
+          <PhotoList />
+        )}
       </main>
     </div>
   );
